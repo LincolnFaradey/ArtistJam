935a2ea94741a2322fe01c0d3b2b674d953b2a0c
Andrei Nechaev <anechaev@nuxeo.com>
2019-01-16 14:34:22 -0500

    able to run

--- Pods/Target Support Files/Pods-ArtistJam/Pods-ArtistJam-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ArtistJam : NSObject
-@end
-@implementation PodsDummy_Pods_ArtistJam
-@end
--- Pods/Target Support Files/Pods-ArtistJamUITests/Pods-ArtistJamUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ArtistJamUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ArtistJamUITests
-@end
